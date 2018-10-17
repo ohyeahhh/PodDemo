9acca8156ccd32b022a4371ae2d6db8e015b2b1a
ohyeah <ohyeahmisoh@gmail.com>
2018-10-17 14:32:18 +0800

    提交代码文件

---
+++ StaticLibPodDemo/StaticLibPodDemo/StaticLibPodDemo.h
@@ -0,0 +1,16 @@
+//
+//  StaticLibPodDemo.h
+//  StaticLibPodDemo
+//
+//  Created by ohyeah on 2018/10/12.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticLibPodDemo : NSObject
+
++ (void)saySomething;
+
+
+@end
---
+++ StaticLibPodDemo/StaticLibPodDemo/StaticLibPodDemo.m
@@ -0,0 +1,17 @@
+//
+//  StaticLibPodDemo.m
+//  StaticLibPodDemo
+//
+//  Created by ohyeah on 2018/10/12.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import "StaticLibPodDemo.h"
+
+@implementation StaticLibPodDemo
+
++(void)saySomething{
+    NSLog(@"I'm a static library.");
+}
+
+@end
